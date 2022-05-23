4035fa325295b8e9a7e8836f5e80b67037baa013
Niloy-Dey <niloydeyce@gmail.com>
2022-05-24 01:03:49 +0600

    dashboard work is done

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -6,11 +6,11 @@ import auth from '../firebase.init';
 
 const Navbar = () => {
 
-    const [user, loading, error] = useAuthState(auth);
-    const logOut = () =>{
+    const [user, gUser, loading, error] = useAuthState(auth);
+    const logOut = () => {
         signOut(auth);
         // localStorage.removeItem('accessToken');
-      }
+    }
 
     return (
         <div className=''>
@@ -23,11 +23,13 @@ const Navbar = () => {
                         <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <li><NavLink to="/home" >Home</NavLink></li>
                             <li><NavLink to="/allTools" >All Tools</NavLink></li>
-                            <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-                            <li><NavLink to="/login">Login</NavLink></li>
+                            {
+                                user && <li><Link to="/dashboard">Dashboard</Link></li>
+                            }
+                            {/* <li><NavLink to="/login">Login</NavLink></li> */}
                             <li><NavLink to="/blog">Blog</NavLink></li>
                             <li><NavLink to="/addProduct">Add Product</NavLink></li>
-                            <li>{user ? <button className="" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link> }</li>
+                            <li>{user ? <button className="" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link>}</li>
 
                         </ul>
                     </div>
@@ -37,19 +39,21 @@ const Navbar = () => {
                     <ul className="menu menu-horizontal p-0">
                         <li><NavLink to="/home" >Home</NavLink></li>
                         <li><NavLink to="/allTools" >All Tools</NavLink></li>
-                        <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-                        <li><NavLink to="/login">Login</NavLink></li>
+                        {
+                            user && <li><Link to="/dashboard">Dashboard</Link></li>
+                        }
+                        {/* <li><NavLink to="/login">Login</NavLink></li> */}
                         <li><NavLink to="/blog">Blog</NavLink></li>
                         <li><NavLink to="/addProduct">Add Product</NavLink></li>
-                        <li>{user ? <button className="" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link> }</li>
+                        <li>{user ? <button className="" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link>}</li>
 
                     </ul>
                 </div>
                 <div className="navbar-end">
-                <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost  lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-            </div>
+                    <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost  lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                </div>
             </div>
         </div>
     );
